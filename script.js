e0180d54d5a19f3beede61abab48b7dfbfdfddca
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-07-15 06:07:08 -0700

    Throw , Try , Catch

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// throw is use to make custom error its syntax is throw "  "  throw syntaxerror ("hshshs")
+
+let employs = ["ali", "kamran","janat","hamza","mohid" ,28];
+if(typeof employs != "string"){
+    throw "not valid"
+}
+
+
+// try , catch , finally 
+// try  { any code full of code}
+// catch(error){ any console like    console.log(error)}
+// finally{ most use in functions like in return the script will reverce but not in finaly }
+
+try{
+    let cars = {
+        name : "audi",
+        color : "blue"
+    }
+       console.log(cars.color);
+    }catch(error){
+    console.log(error)
+}
+
+
+
+
+
 import { Students, people } from "./abc.js";
 
 let studentss = new Students("ali", 23)
@@ -7,5 +34,7 @@ console.log(people);
 console.log(Object.entries(people));
 console.log(Object.values(people));
 
-// let studentw = new ("alii", 64);
-// studentw.name();
+
+
+let studentw = new ("alii", 64);
+studentw.name(); 
